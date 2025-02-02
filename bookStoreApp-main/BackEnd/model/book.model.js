5a0d2846d5c67a27fbd169fec5a2e176f286efd8
Usaira Shahbaz Zahid <145522680+Usaira-Shahbaz@users.noreply.github.com>
2025-02-02 22:10:32 +0500

    MERN Application

---
+++ bookStoreApp-main/BackEnd/model/book.model.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const bookSchema = mongoose.Schema({
+    name: String,
+    price: Number,
+    category: String,
+    image: String,
+    title: String,
+});
+const Book = mongoose.model("seasons", bookSchema);
+
+export default Book;
